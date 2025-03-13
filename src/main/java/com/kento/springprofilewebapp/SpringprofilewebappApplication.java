cca4e97b2a2cb39d9194ca7d692ecabce337d5b7
Kento103 <kento103@outlook.com>
2025-03-14 00:05:36 +0900

    新規作成(Init)
    Springbootアプリの新規作成を行った、デフォルト状態から変更なし。

---
+++ src/main/java/com/kento/springprofilewebapp/SpringprofilewebappApplication.java
@@ -0,0 +1,13 @@
+package com.kento.springprofilewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprofilewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprofilewebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kento/springprofilewebapp/SpringprofilewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kento.springprofilewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprofilewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
