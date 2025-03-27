1473d02d21b15bba252492febdc88384235d4661
Kento103 <kento103@outlook.com>
2025-03-28 00:48:47 +0900

    ユーザー情報編集ページの追加、編集及び不具合の修正を行った(Add modified fix)
    追加：ユーザ一覧ページの追加
    編集：編集画面がどのユーザーでも誰でも編集できる状態になっていた為、適切な権限を設定した。
    修正：表示が上手くされていなかったところの修正作業を行った。

--- src/main/java/com/kento/springprofilewebapp/controller/UserController.java
+++ src/main/java/com/kento/springprofilewebapp/controller/UserController.java
@@ -1,18 +1,14 @@
 package com.kento.springprofilewebapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.kento.springprofilewebapp.model.Users;
 import com.kento.springprofilewebapp.service.UserService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,10 +31,21 @@ public class UserController {
 
     // ユーザー情報の編集ページを表示する
     @GetMapping("/{id}/edit")
-    public String editUserPage(@PathVariable int id, Model model, Users user) {
+    public String editUserPage(@PathVariable int id, Model model, Users user, @AuthenticationPrincipal Users loginUser) {
         user = userService.getUserById(id); // usersテーブルのidから該当の情報を検索する
         model.addAttribute("user", user); // 情報をモデルへ代入する(thymaleefで使えるようにする)
-        return "useredit"; //useredit.htmlのページを表示する
+        if (loginUser.getRole().equals("ROLE_ADMIN")) { // 自分がROLE_ADMIN保有者か確認するloginUserはオブジェクトなので#.equalsでないと不一致となる(==ではだめ)
+            // 管理者権限ロールでログイン中の場合はどのユーザの編集もできる
+            return "useredit"; // useredit.htmlのページを表示する
+        } else {
+            // それ以外のユーザは自分のみの編集ができる
+            if (user.getId() == loginUser.getId()) { // user.getId()...閲覧中のUserID loginUser.getId()...ログイン中のユーザーID
+                return "useredit"; // useredit.htmlのページを表示する
+            } else {
+                model.addAttribute("error", "権限がありません");
+                return "redirect:/users/{id}"; // プロフィールに戻す
+            }
+        }
     }
 
     // ユーザー情報の更新を行うもの
@@ -48,10 +55,11 @@ public class UserController {
             @ModelAttribute Users user,
             Model model,
             @ModelAttribute Users users,
+            @AuthenticationPrincipal Users loginUsers,
             BindingResult bindingResult) {
             if (bindingResult.hasErrors()) {
                 //Getリクエスト用のメゾットを呼び出し、編集画面に戻る
-                return editUserPage(user.getId(), model, user);
+                return editUserPage(user.getId(), model, user, loginUsers);
             }
             try {
                 users = userService.updateUser(id, user);
@@ -62,4 +70,10 @@ public class UserController {
                 return "useredit";
             }
         }
+    
+    // ユーザーリスト一覧
+    @GetMapping
+    public String userList() {
+        return "userlist";
+    }
 }
